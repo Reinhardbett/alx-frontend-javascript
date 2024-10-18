97c1f4bef3abc56adcc3a8c80d7c9b8a0dbf2701
Reinhardbett <bettkiptoo46@gmail.com>
2024-10-18 05:20:17 -0700

    feat: use reduce function on list of objs

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,11 @@
+// Return sum of all student ids
+
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
+  }
+  return 0;
+}
