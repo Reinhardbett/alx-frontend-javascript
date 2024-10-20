6ed9927ca8fef9c1ce989672c4e86f13e1101024
Reinhardbett <bettkiptoo46@gmail.com>
2024-10-20 05:01:15 -0700

    feat: use object value shorthand syntax

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,6 +1,5 @@
 export default function getBudgetObject(income, gdp, capita) {
   const budget = {
-    // Use ES6 object shorthand syntax to assign values on function call
     income,
     gdp,
     capita,
