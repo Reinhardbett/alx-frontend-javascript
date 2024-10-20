5c868697989659147f28f5e708dd6ed86e548670
Reinhardbett <bettkiptoo46@gmail.com>
2024-10-20 08:40:02 -0700

    feat: shorthand syntax for ES6 method properties

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,17 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    // ES6 allows you to define methods directly without needing to use the function keyword
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
