bd54ed48830f854cab678d3ca5f858098a16376a
Reinhardbett <bettkiptoo46@gmail.com>
2024-10-20 04:01:17 -0700

    feat: Create Map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+// Return a map of groceries with format (name, quantity)
+
+export default function groceriesList() {
+  const myMap = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+  return myMap;
+}
