61a766bd70ac47ab4e50a14fa7f501245f03dbd9
Reinhardbett <bettkiptoo46@gmail.com>
2024-10-21 05:35:08 -0700

    feat: Create a reporting object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+// Create a report object
+
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
