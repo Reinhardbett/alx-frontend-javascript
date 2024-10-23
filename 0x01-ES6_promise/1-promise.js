4278605ee031ceca2fe83322f643b865dfdfa678
Reinhardbett <bettkiptoo46@gmail.com>
2024-10-23 12:12:36 -0700

    feat: resolve and reject promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+// Resolve and reject promise depending on input
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
