ed91de211e6a333f7aa34955cda18f02e744cbfc
Reinhardbett <bettkiptoo46@gmail.com>
2024-10-24 04:43:51 -0700

    feat: reject a promise with a custom error message

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,5 @@
+// Return a Promise rejecting with an Error and the string $fileName cannot be processed
+
+export default function uploadPhoto(filename) {
+  return Promise.reject(new Error(`${filename} cannot be processed`));
+}
